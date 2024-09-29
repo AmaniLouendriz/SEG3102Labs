a0fb50a10fab43c949611f8216b3afb49d880f48
Amani <alouendr@uottawa.ca>
2024-09-29 09:44:25 -0400

    updates

--- lab3-AmaniLouendriz/src/app/adder/item-entry.spec.ts
+++ lab3-AmaniLouendriz/src/app/adder/item-entry.spec.ts
@@ -2,6 +2,6 @@ import { ItemEntry } from './item-entry';
 
 describe('ItemEntry', () => {
   it('should create an instance', () => {
-    expect(new ItemEntry()).toBeTruthy();
+    expect(new ItemEntry(1,'')).toBeTruthy();
   });
 });
